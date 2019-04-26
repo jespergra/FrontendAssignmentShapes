97f48c56cd2e8475b8e80b753dda3ad356596fd3
Jesper Grandien <jesperg.1997@live.se>
2019-04-26 22:36:44 +0100

    moving points now working

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 
 // Constructor for Shape objects to hold data for all drawn objects.
 // For now they will just be defined as rectangles.
-var shapes;
+//var shapes;
 function Shape(x, y, w, h, fill) {
 	// This is a very simple and unsafe constructor. All we're doing is checking if the values exist.
 	// "x || 0" just means "if there is a value for x, use that. Otherwise use 0."
@@ -22,14 +22,14 @@ function Shape(x, y, w, h, fill) {
 }
 
 // Sorting by X-coordinate
-function sortFunction(a, b) {
-    if (a[0] === b[0]) {
-        return 0;
-    }
-    else {
-        return (a[0] < b[0]) ? -1 : 1;
-    }
-}
+// function sortFunction(a, b) {
+//     if (a[0] === b[0]) {
+//         return 0;
+//     }
+//     else {
+//         return (a[0] < b[0]) ? -1 : 1;
+//     }
+// }
 
 // Draws this shape to a given context
 Shape.prototype.draw = function (ctx) {
@@ -45,6 +45,7 @@ function drawParalellogram (ctx) {
 	ctx.globalCompositeOperation = 'destination-over';
 	for (i = 0; i < 4; i++) {
 		console.log(i);
+		ctx.beginPath();
 		ctx.moveTo(shapes[i].x, shapes[i].y);
 		if (i !== 3) {
 			ctx.lineTo(shapes[i + 1].x, shapes[i + 1].y);
@@ -54,6 +55,39 @@ function drawParalellogram (ctx) {
 		ctx.strokeStyle = "#0000FF";
 		ctx.stroke();
 	}
+
+	var middleX = (shapes[0].x + shapes[2].x) / 2;
+	var middleY = (shapes[1].y + shapes[3].y) / 2;
+	ctx.beginPath();
+
+	// Use first point as zero
+	var aX = shapes[1].x - shapes[0].x;
+	var aY = shapes[1].y - shapes[0].y;
+	var bX = shapes[3].x - shapes[0].x;
+	var bY = shapes[3].y - shapes[0].y;
+	console.log("ax:" + aX);
+	console.log("ay:" + aY);
+	console.log("bx:" + bX);
+	console.log("by:" + bY);
+
+	// Area of the parallelogram using determinants
+	var pArea = Math.abs((aX * bY) - (aY * bX));
+
+	console.log("pArea: " + pArea);
+	var radius = Math.sqrt((pArea / Math.PI));
+	console.log("radius: " + radius);
+	var cArea = radius * radius * Math.PI;
+	console.log("circle area: " + cArea);
+	ctx.arc(middleX, middleY, radius, 0, 2 * Math.PI);
+	ctx.strokeStyle = "#FFFF00";
+	ctx.stroke();
+
+	info.innerHTML = "Paralelloram area: " + pArea + "px&sup2;<br />Circle area: " + cArea + "px&sup2;<br />" +
+		"Coords point 1: " + shapes[0].x + ", " + shapes[0].y + "<br />" +
+		"Coords point 2: " + shapes[1].x + ", " + shapes[1].y + "<br />" +
+		"Coords point 3: " + shapes[2].x + ", " + shapes[2].y + "<br />" +
+		"Coords point 4: " + shapes[3].x + ", " + shapes[3].y + "<br />";
+
 }
 
 // Determine if a point is inside the shape's bounds
@@ -145,6 +179,8 @@ function CanvasState(canvas) {
 			// from where we clicked. Thats why we saved the offset and use it here
 			myState.selection.x = mouse.x - myState.dragoffx;
 			myState.selection.y = mouse.y - myState.dragoffy;
+			shapes[3].x = shapes[0].x + shapes[2].x - shapes[1].x;
+			shapes[3].y = shapes[0].y + shapes[2].y - shapes[1].y;
 			myState.valid = false; // Something's dragging so we must redraw
 		}
 	}, true);
@@ -153,41 +189,48 @@ function CanvasState(canvas) {
 	}, true);
 	// double click for making new shapes
 
+	document.getElementById("reset").addEventListener('click', function() {
+		myState.ctx.clearRect(0, 0,  c.width, c.height);
+		count = 0;
+		myState.shapes = [];
+		info.innerHTML = "Reseting..";
+	}, true);
+
 	canvas.addEventListener('click', function (e) {
 		var mouse = myState.getMouse(e);
 		count++;
 		if (count < 4) {
 			console.log("count:" + count);
-			coordinates.push([mouse.x, mouse.y]);
+			//coordinates.push([mouse.x, mouse.y]);
 			myState.addShape(new Shape(mouse.x, mouse.y, 11, 11, 'red'));
 			if (count == 3) {
 				var fourthX = shapes[0].x + shapes[2].x - shapes[1].x;
 				var fourthY = shapes[0].y + shapes[2].y - shapes[1].y;
 				// draw fourth vertex
 				ctx.beginPath();
-				coordinates.push([fourthX, fourthY]);
+				//coordinates.push([fourthX, fourthY]);
 				var highestX = 0;
 				var highestY = 0;
 				var lowestX = 9999;
 				var lowestY = 9999;
-				coordinates.forEach(function (coordinate) {
-					if (coordinate[0] > highestX) {
-						highestX = coordinate[0];
+				shapes.forEach(function (shape) {
+					if (shape.x > highestX) {
+						highestX = shape.x;
 					}
-					if (coordinate[1] > highestY) {
-						highestY = coordinate[1];
+					if (shape.y > highestY) {
+						highestY = shape.y;
 					}
-					if (coordinate[0] < lowestX) {
-						lowestX = coordinate[0];
+					if (shape.x < lowestX) {
+						lowestX = shape.x;
 					}
-					if (coordinate[1] < lowestY) {
-						lowestY = coordinate[1];
+					if (shape.y < lowestY) {
+						lowestY = shape.y;
 					}
 				});
 				var middleCoords = [];
-				coordinates.forEach(function (coordinate) {
-					if (coordinate[0] < highestX && coordinate[0] > lowestX) {
-						middleCoords.push(coordinate);
+				shapes.forEach(function (shape) {
+					if (shape.x < highestX && shape.x > lowestX) {
+						middleCoords.push(shape);
 					}
 				});
 				console.log("highestX: " + highestX);
@@ -195,72 +238,7 @@ function CanvasState(canvas) {
 				console.log("lowestX: " + lowestX);
 				console.log("lowestY: " + lowestY);
 				console.log(middleCoords);
-				// if (middleCoords[0][0] > middleCoords[1][0]) {
-				//     var b = middleCoords[0];
-				//     middleCoords[0] = middleCoords[1];
-				//     middleCoords[1] = b;
-				// }
-				console.log(middleCoords);
-				// var pArea = Math.pow(middleCoords[0][0] - middleCoords[1][0], 2) + Math.pow(middleCoords[0][1] - middleCoords[1][1], 2);
-				// pArea = Math.sqrt(pArea);
-				// console.log(pArea);
-				//ctx.moveTo(fourthX, fourthY);
-				// ctx.strokeStyle = "#00FF00";
-				// ctx.arc(fourthX, fourthY, 5.5, 0, 2 * Math.PI);
-				// ctx.fillStyle = "#00FF00";
-				// ctx.fill();
-				// ctx.stroke();
 				myState.addShape(new Shape(fourthX, fourthY, 11, 11, '#00FF00'));
-				// draw paralellogram, toDo: function of this
-				console.log(coordinates);
-				ctx.globalCompositeOperation = 'destination-over';
-				for (i = 0; i < 4; i++) {
-					console.log(i);
-					ctx.moveTo(coordinates[i][0], coordinates[i][1]);
-					if (i !== 3) {
-						ctx.lineTo(coordinates[i + 1][0], coordinates[i + 1][1]);
-					} else {
-						ctx.lineTo(coordinates[0][0], coordinates[0][1]);
-					}
-					ctx.strokeStyle = "#0000FF";
-					ctx.stroke();
-				}
-				var middleX = (coordinates[0][0] + coordinates[2][0]) / 2;
-				var middleY = (coordinates[1][1] + coordinates[3][1]) / 2;
-				ctx.beginPath();
-
-				// Save the original coordinates for points to remember in what order they have been placed.
-				var originalCoordinates = coordinates;
-				//coordinates.sort(sortFunction);
-				console.log(coordinates);
-
-				// Use first point as zero
-				var aX = coordinates[1][0] - coordinates[0][0];
-				var aY = coordinates[1][1] - coordinates[0][1];
-				var bX = coordinates[3][0] - coordinates[0][0];
-				var bY = coordinates[3][1] - coordinates[0][1];
-				console.log("ax:" + aX);
-				console.log("ay:" + aY);
-				console.log("bx:" + bX);
-				console.log("by:" + bY);
-
-				// Area of the parallelogram using determinants
-				var pArea = Math.abs((aX * bY) - (aY * bX));
-
-				console.log("pArea: " + pArea);
-				var radius = Math.sqrt((pArea / Math.PI));
-				console.log("radius: " + radius);
-				var cArea = radius * radius * Math.PI;
-				console.log("circle area: " + cArea);
-				ctx.arc(middleX, middleY, radius, 0, 2 * Math.PI);
-				ctx.strokeStyle = "#FFFF00";
-				ctx.stroke();
-
-				info.innerHTML = "Paralelloram area: " + pArea + "px&sup2;<br />Circle area: " + cArea + "px&sup2;<br />" +
-					"Coords point 1: " + originalCoordinates[0][0] + ", " + originalCoordinates[0][1] + "<br />" +
-					"Coords point 2: " + originalCoordinates[1][0] + ", " + originalCoordinates[1][1] + "<br />" +
-					"Coords point 3: " + originalCoordinates[2][0] + ", " + originalCoordinates[2][1] + "<br />" +
-					"Coords point 4: " + originalCoordinates[3][0] + ", " + originalCoordinates[3][1] + "<br />";
 			}
 		}
 	}, true);
@@ -290,7 +268,7 @@ CanvasState.prototype.draw = function () {
 		var ctx = this.ctx;
 		var shapes = this.shapes;
 		this.clear();
-
+		
 		// ** Add stuff you want drawn in the background all the time here **
 
 		// draw all shapes
@@ -350,6 +328,7 @@ CanvasState.prototype.getMouse = function (e) {
 
 // If you dont want to use <body onLoad='init()'>
 // You could uncomment this init() reference and place the script reference inside the body tag
+
 init();
 
 function init() {
@@ -361,3 +340,4 @@ function init() {
 }
 
   // Now go make something amazing!
+
